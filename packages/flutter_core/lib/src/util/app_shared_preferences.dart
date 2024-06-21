955cdafa4ec581f2c4eaf02367d411115a72dfe0
Doan Van Ngoc/대리/메가존_MZ HMI <dvngoc@mz.co.kr>
2024-06-21 10:34:15 +0700

    Update

--- packages/flutter_core/lib/src/util/app_shared_preferences.dart
+++ packages/flutter_core/lib/src/util/app_shared_preferences.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart' as sp;
 
 class AppSharedPreferences {
@@ -7,30 +10,44 @@ class AppSharedPreferences {
     required this.prefs,
   });
 
-  Object? get(String key) => prefs.get(key);
+  T? get<T>(String key, [T? defaultValue]) {
+    final value = prefs.get(key);
+    if (value is T) {
+      return value;
+    }
 
-  bool? getBool(String key) => prefs.getBool(key);
+    try {
+      var decodedValue = jsonDecode(value.toString());
+      if (decodedValue is T) {
+        return decodedValue;
+      }
+    } on Exception catch (e) {
+      if (kDebugMode) print(e);
+    }
 
-  int? getInt(String key) => prefs.getInt(key);
-
-  double? getDouble(String key) => prefs.getDouble(key);
-
-  String? getString(String key) => prefs.getString(key);
+    return defaultValue;
+  }
 
   bool containsKey(String key) => prefs.containsKey(key);
 
-  Future<bool> setBool(String key, bool value) => prefs.setBool(key, value);
-
-  Future<bool> setInt(String key, int value) => prefs.setInt(key, value);
-
-  Future<bool> setDouble(String key, double value) =>
-      prefs.setDouble(key, value);
-
-  Future<bool> setString(String key, String value) =>
-      prefs.setString(key, value);
-
-  Future<bool> setStringList(String key, List<String> value) =>
-      prefs.setStringList(key, value);
+  Future<bool> set<T>(String key, T? value) {
+    switch (T) {
+      case String:
+        return prefs.setString(key, value as String);
+      case int:
+        return prefs.setInt(key, value as int);
+      case bool:
+        return prefs.setBool(key, value as bool);
+      case double:
+        return prefs.setDouble(key, value as double);
+      case const (List<String>):
+        return prefs.setStringList(key, value as List<String>);
+      default:
+        return Future(() {
+          return jsonEncode(value);
+        }).then((value) => prefs.setString(key, value));
+    }
+  }
 
   Future<bool> remove(String key) => prefs.remove(key);
 
