e1a3481ecf3e5a0c273413ac83c47d461f06488f
Doan Van Ngoc/대리/메가존_MZ HMI <dvngoc@mz.co.kr>
2024-07-15 09:42:06 +0700

    Update database

--- packages/flutter_core/lib/src/util/app_database_helper.dart
+++ packages/flutter_core/lib/src/util/app_database_helper.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
+
 import '../../flutter_core.dart';
 
 class AppDatabaseHelper {
@@ -11,7 +13,7 @@ class AppDatabaseHelper {
   final OnDatabaseVersionChangeFn? onDowngrade;
   final Function(Database db)? onOpen;
 
-  final Map<String, StreamController<dynamic>> _controllers = {};
+  final Map<QueryOptions<dynamic>, StreamController<dynamic>> _controllers = {};
 
   AppDatabaseHelper(
       {required this.dbName,
@@ -31,9 +33,153 @@ class AppDatabaseHelper {
         onOpen: (db) => {onOpen?.call(db)});
   }
 
-  void _runDatabase(Function(Database db) action) async {
+  Future<Database> _getDatabase() async {
     _database ??= await initDatabase();
     var db = _database;
-    if (db != null) action(db);
+    return db!;
+  }
+
+  Future<List<T>> query<T>(QueryOptions<T> options) async {
+    var db = await _getDatabase();
+    var list = await db.query(options.table,
+        distinct: options.distinct,
+        columns: options.columns,
+        where: options.where,
+        whereArgs: options.whereArgs,
+        groupBy: options.groupBy,
+        having: options.having,
+        orderBy: options.orderBy,
+        limit: options.limit,
+        offset: options.offset);
+
+    return list.map(options.mapper).toList();
+  }
+
+  Stream<List<T>?> queryStream<T>(QueryOptions<T> options) {
+    if (!_controllers.containsKey(options)) {
+      _controllers[options] = StreamController<T?>.broadcast(
+        onListen: () {
+          _controllers[options]?.add(query(options));
+        },
+      );
+    }
+
+    return _controllers[options]?.stream as Stream<List<T>?>;
+  }
+
+  Future<int> update(String table, Map<String, dynamic> data, String where,
+      List<dynamic> whereArgs) async {
+    Database db = await _getDatabase();
+    var id = await db.update(table, data, where: where, whereArgs: whereArgs);
+    _notifyControllers(table, "update");
+    return id;
+  }
+
+  Future<int> delete(String table,
+      {String? where, List<dynamic>? whereArgs}) async {
+    Database db = await _getDatabase();
+    var id = await db.delete(table, where: where, whereArgs: whereArgs);
+    _notifyControllers(table, "delete");
+    return id;
+  }
+
+  Future<int> insert(String table, Map<String, dynamic> data,
+      {String? nullColumnHack, ConflictAlgorithm? conflictAlgorithm}) async {
+    Database db = await _getDatabase();
+    var id = await db.insert(table, data,
+        nullColumnHack: nullColumnHack,
+        conflictAlgorithm: conflictAlgorithm ?? ConflictAlgorithm.replace);
+    _notifyControllers(table, "insert");
+    return id;
+  }
+
+  void _notifyControllers(String table, String action) async {
+    _controllers.forEach((options, controller) {
+      if (options.table == table) {
+        controller.add(query(options));
+      }
+    });
+  }
+
+  void dispose() {
+    _controllers.forEach((key, a) {
+      if (!a.isClosed) a.close();
+    });
+
+    _controllers.clear();
+  }
+
+  void close({String? table, QueryOptions<dynamic>? options}) {
+    _controllers.forEach((key, a) {
+      if ((table != null && key.table == table) ||
+          key.table == options?.table) {
+        if (!a.isClosed) {
+          a.close();
+        }
+        _controllers.remove(key);
+        return;
+      }
+    });
+  }
+}
+
+class QueryOptions<T> {
+  final String table;
+  final T Function(Map<String, dynamic>) mapper;
+  final bool? distinct;
+  final List<String>? columns;
+  final String? where;
+  final List<Object?>? whereArgs;
+  final String? groupBy;
+  final String? having;
+  final String? orderBy;
+  final int? limit;
+  final int? offset;
+
+  QueryOptions({
+    required this.table,
+    required this.mapper,
+    this.distinct,
+    this.columns,
+    this.where,
+    this.whereArgs,
+    this.groupBy,
+    this.having,
+    this.orderBy,
+    this.limit,
+    this.offset,
+  });
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is QueryOptions<T> &&
+        other.table == table &&
+        other.mapper == mapper &&
+        other.distinct == distinct &&
+        listEquals(other.columns, columns) &&
+        other.where == where &&
+        listEquals(other.whereArgs, whereArgs) &&
+        other.groupBy == groupBy &&
+        other.having == having &&
+        other.orderBy == orderBy &&
+        other.limit == limit &&
+        other.offset == offset;
+  }
+
+  @override
+  int get hashCode {
+    return table.hashCode ^
+        mapper.hashCode ^
+        distinct.hashCode ^
+        columns.hashCode ^
+        where.hashCode ^
+        whereArgs.hashCode ^
+        groupBy.hashCode ^
+        having.hashCode ^
+        orderBy.hashCode ^
+        limit.hashCode ^
+        offset.hashCode;
   }
 }
